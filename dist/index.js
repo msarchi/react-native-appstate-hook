beb23274f7dd5e0327dbd77f507bbd7ae9670675
msarchi <msarchi@gmail.com>
2022-02-13 15:49:16 +0100

    feat: call callbacks on startup

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,38 @@
-import { useState, useEffect } from 'react';
-import { AppState } from 'react-native';
+import { useState, useEffect } from "react";
+import { AppState } from "react-native";
+
+// settings validation
+function isValidFunction(func) {
+  return func && typeof func === "function";
+}
 
 export default function useAppState(settings) {
   const { onChange, onForeground, onBackground } = settings || {};
-  const [appState, setAppState] = useState(AppState.currentState);
+  const [appState, setAppState] = useState();
 
   useEffect(() => {
     function handleAppStateChange(nextAppState) {
-      if (nextAppState === 'active' && appState !== 'active') {
+      if (nextAppState === "active" && appState !== "active") {
         isValidFunction(onForeground) && onForeground();
-      } else if (appState === 'active' && nextAppState.match(/inactive|background/)) {
+      } else if (
+        appState === "active" &&
+        nextAppState.match(/inactive|background/)
+      ) {
         isValidFunction(onBackground) && onBackground();
       }
       setAppState(nextAppState);
       isValidFunction(onChange) && onChange(nextAppState);
     }
-    const appState = AppState.addEventListener('change', handleAppStateChange);
 
-    return () => appState.remove();
-  }, [onChange, onForeground, onBackground, appState]);
+    handleAppStateChange(AppState.currentState);
 
-  // settings validation
-  function isValidFunction(func) {
-    return func && typeof func === 'function';
-  }
+    const appStateListener = AppState.addEventListener(
+      "change",
+      handleAppStateChange
+    );
+
+    return () => appStateListener.remove();
+  }, []);
+
   return { appState };
 }
